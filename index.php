ea0479f5bf13414dbaced39acaa6e3268f579999
DPCoderr <s1173932@student.windesheim.nl>
2023-09-17 16:53:21 +0200

    Final work

--- index.php
+++ index.php
@@ -3,10 +3,12 @@
 require 'functions/function.php';
 
 /**
- * Maker: Daniel Pustjens (AD Software Development Student Year 1)
+ * Group Underdogs: 
+ *  - Daniel Pustjens (AD Software Development Student Year 1).
+ *  - Jordy Meijer (AD Software Development Student Year 1).
+ *  - Omar Pustjens (AD Software Development Student Year 1).
  * 
  * Homework software language 1 (week 2):
- * 
  * - Make an array with all your members variables. 
  * - Echo the array using a for loop and foreach loop.
  * 
@@ -24,47 +26,48 @@ $group = [
         'Colleg year' => 'Year 1',
         'Family' => 'Father, Mother',
         'Languages' => 'PHP, Javascript, HTML, CSS, C',
-        'Hobbies' => 'Work-out, Game, Sleep, Boxing, MMA',
+        'Hobbies' => 'Work-out, Game, Sleep, Boxing, MMA'
     ],
     [
-        'Name' => 'Jordy',
-        'Age' => 22,
-        'Mobile' => 31615601397,
-        'Email' => 'daniel.pustjens@gmail.com',
+        'Name' => 'Jordy Meijer',
+        'Age' => 19,
+        'Mobile' => 31634271862,
+        'Email' => 'jmeijer04@hotmail.com',
         'College name' => 'Windesheim',
         'College study' => 'AD Software Development',
         'College year' => 'Year 1',
-        'Family' => 'Father, Mother',
-        'Languages' => 'PHP, Javascript, HTML, CSS, C',
-        'Hobbies' => 'Work-out, Game, Sleep, Boxing, MMA',
+        'Family' => 'Father, Mother', 'Brother',
+        'Languages' => 'PHP, Javascript, HTML, CSS',
+        'Hobbies' => 'Work-out, Gaming, Reading, Coding'
     ],
     [
-        'Name' => 'Omar',
-        'Age' => 22,
-        'Mobile' => 31615601397,
-        'Email' => 'daniel.pustjens@gmail.com',
+        'Name' => 'Omar Omicevic',
+        'Age' => 21,
+        'Mobile' => 31646571019,
+        'Email' => 'omarcevic2002@gmail.com',
         'College name' => 'Windesheim',
         'College study' => 'AD Software Development',
         'College year' => 'Year 1',
-        'Family' => 'Father, Mother',
-        'Languages' => 'PHP, Javascript, HTML, CSS, C',
-        'Hobbies' => 'Work-out, Game, Sleep, Boxing, MMA',
+        'Family' => 'Father, Mother, Sister',
+        'Languages' => 'PHP, Javascript, HTML, CSS',
+        'Hobbies' => 'Gaming, Cycling, Fortnite Battle Pass'
     ]
 ];
 
-echo "Array for 1 group: <br><br>";
-// Loop the whole group array that has 3 members arrays inside of it.
+echo "<i>Array for 1 group: </i><br><br>";
+echo "<b>Group: Underdogs </b><br><br>";
+// In the group array are multiple arrays for every member.
 foreach ($group as $member) {
-    // Loop each member array which is an associative array.
+    // Each member array is an associative array with their information stored in there.
     foreach ($member as $key => $value) {
         echo $key . ": " . $value . "<br>";
     }
     echo "<hr>";
 }
 
-// Loop the whole group array that has 3 members arrays inside of it.
+// In the group array are multiple arrays for every member.
 for ($i = 0; $i < count($group); $i++) {
-    // Each member has an associative array, which means you need to know 
+    // Each member array is an associative array, which means you need to know 
     // how many keys/values there are to know how many times the for loop has to loop.
     $keysMember = array_keys($group[$i]);
     $valuesMember = array_values($group[$i]);
@@ -79,7 +82,7 @@ echo "<hr><hr>";
 $groups = [
     'Underdogs' => [
         [
-            'Name' => 'Daniel',
+            'Name' => 'Daniel Pustjens',
             'Age' => 22, 
             'Mobile' => 31615601397,
             'Email' => 'daniel.pustjens@gmail.com',
@@ -91,7 +94,7 @@ $groups = [
             'Hobbies' => 'Work-out, Gaming, Sleep, Boxing, MMA'
         ],
         [
-            'Name' => 'Jordy',
+            'Name' => 'Jordy Meijer',
             'Age' => 19,
             'Mobile' => 31634271862,
             'Email' => 'jmeijer04@hotmail.com',
@@ -103,7 +106,7 @@ $groups = [
             'Hobbies' => 'Work-out, Gaming, Reading, Coding'
         ],
         [
-            'Name' => 'Omar',
+            'Name' => 'Omar Omicevic',
             'Age' => 21,
             'Mobile' => 31646571019,
             'Email' => 'omarcevic2002@gmail.com',
@@ -111,20 +114,20 @@ $groups = [
             'College study' => 'AD Software Development',
             'College year' => 'Year 1',
             'Family' => 'Father, Mother, Sister',
-            'Languages' => 'PHP, Javascript, HTML, CSS, C',
-            'Hobbies' => 'Gaming, Cycling, Fortnite battle pass'
+            'Languages' => 'PHP, Javascript, HTML, CSS',
+            'Hobbies' => 'Gaming, Cycling, Fortnite Battle Pass'
         ]
     ]
 ];
 
-echo "Array for multiple groups: <br><br>";
-// Array $groups needs to loop first an associative array (key: group, value: information group members).
-foreach ($groups as $group => $member) {
-    echo $group . "<br>";
-    // The information value is kept in an array with other arrays in there for every member, so a normal foreach loop.
-    foreach ($member as $infoMember) {
-        // The information for each member is kept in associative arrays.
-        foreach ($infoMember as $key => $value) {
+echo "<i>Array for multiple groups: </i><br><br>";
+// Array $groups is an associative array (key: group, value: group members).
+foreach ($groups as $group => $members) {
+    echo "<b>Group: ". $group . "</b><br><br>";
+    // Each member array is an associative array with their information stored in there.
+    foreach ($members as $member) {
+        // The information for each member is kept in an associative array.
+        foreach ($member as $key => $value) {
             echo $key . ": " . $value . "<br>";
         }
         echo "<hr>";
